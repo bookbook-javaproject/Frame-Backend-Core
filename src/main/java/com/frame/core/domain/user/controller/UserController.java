62c617a43b4cc7fd4ac4e522f060ac236ee2baef
Maly <migsking@naver.com>
2020-10-21 12:42:36 +0900

    [ADD] #5 Create Account Controller

--- src/main/java/com/frame/core/domain/user/controller/UserController.java
+++ src/main/java/com/frame/core/domain/user/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.frame.core.domain.user.controller;
 
 
+import com.frame.core.domain.user.dto.RegisterRequest;
+import com.frame.core.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void registerAccount(@RequestBody @Valid RegisterRequest request) {
+        userService.registerService(request);
+    }
 }
