c3aa5af3ca0dc42f6d6a0bedcda64bb9c07c9906
Maly <migsking@naver.com>
2020-10-21 14:24:49 +0900

    [ADD] #8 Already Exists 확인

--- src/main/java/com/frame/core/domain/user/domain/usecase/CreateUserUseCaseImpl.java
+++ src/main/java/com/frame/core/domain/user/domain/usecase/CreateUserUseCaseImpl.java
@@ -1,6 +1,7 @@
 package com.frame.core.domain.user.domain.usecase;
 
 import com.frame.core.domain.user.domain.entity.User;
+import com.frame.core.domain.user.domain.exception.UserAlreadyExistsException;
 import com.frame.core.domain.user.domain.repository.UserRepository;
 import com.frame.core.domain.user.domain.service.PasswordService;
 import lombok.AllArgsConstructor;
@@ -15,6 +16,10 @@ public class CreateUserUseCaseImpl implements CreateUserUseCase {
 
     @Override
     public void run(String email, String nickname, String password) {
+        userRepository.findById(email).ifPresent(
+            user -> { throw new UserAlreadyExistsException();}
+        );
+
         userRepository.save(
                 User.builder()
                 .email(email)
