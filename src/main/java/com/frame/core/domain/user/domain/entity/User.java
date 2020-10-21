a27d9ae5b55f221e105283942859f34773eea4f0
Maly <migsking@naver.com>
2020-10-21 13:43:37 +0900

    [MODIFY] #3 길이 설정

--- src/main/java/com/frame/core/domain/user/domain/entity/User.java
+++ src/main/java/com/frame/core/domain/user/domain/entity/User.java
@@ -1,6 +1,8 @@
 package com.frame.core.domain.user.domain.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,13 +12,15 @@ import javax.persistence.Table;
 @Builder
 @Entity
 @Table(name = "tbl_user")
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 12)
     private String nickname;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 60)
     private String password;
 }
