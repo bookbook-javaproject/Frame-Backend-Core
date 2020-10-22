a800f4a05dbfcda5ac5c96558c9a59a7be6fde5f
Maly <migsking@naver.com>
2020-10-22 12:32:56 +0900

    [ADD] #8 UpdatePassword Service

--- src/main/java/com/frame/core/domain/user/service/UserService.java
+++ src/main/java/com/frame/core/domain/user/service/UserService.java
@@ -1,7 +1,10 @@
 package com.frame.core.domain.user.service;
 
+import com.frame.core.domain.user.dto.ChangePasswordRequest;
 import com.frame.core.domain.user.dto.RegisterRequest;
 
 public interface UserService {
     public void registerService(RegisterRequest request);
+
+    public void changePasswordService(ChangePasswordRequest request);
 }
