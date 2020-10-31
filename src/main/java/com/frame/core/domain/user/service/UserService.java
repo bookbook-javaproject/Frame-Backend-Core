8fbf7db047a32729819b068842d31ca03926fcd5
Maly <migsking@naver.com>
2020-10-31 23:03:23 +0900

    [ADD] #8 Login Service

--- src/main/java/com/frame/core/domain/user/service/UserService.java
+++ src/main/java/com/frame/core/domain/user/service/UserService.java
@@ -1,10 +1,14 @@
 package com.frame.core.domain.user.service;
 
 import com.frame.core.domain.user.dto.ChangePasswordRequest;
+import com.frame.core.domain.user.dto.LoginRequest;
+import com.frame.core.domain.user.dto.LoginResponse;
 import com.frame.core.domain.user.dto.RegisterRequest;
 
 public interface UserService {
     public void registerService(RegisterRequest request);
+
+    public LoginResponse login(LoginRequest request);
 
     public void changePasswordService(ChangePasswordRequest request);
 }
