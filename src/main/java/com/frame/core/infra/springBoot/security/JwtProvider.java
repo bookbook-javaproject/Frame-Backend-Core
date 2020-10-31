e4f3b17450b304378246d6298f8b278db65b0031
Maly <migsking@naver.com>
2020-10-31 23:04:05 +0900

    [CREATE] #2 JWT Provider

---
+++ src/main/java/com/frame/core/infra/springBoot/security/JwtProvider.java
@@ -0,0 +1,96 @@
+package com.frame.core.infra.springBoot.security;
+
+
+import com.frame.core.global.exceptions.InvalidTokenException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+    @Value("${auth.jwt.secret}")
+    private String secretKey;
+
+    @Value("${auth.jwt.exp.access}")
+    private Long accessTokenExpiration;
+
+    @Value("${auth.jwt.exp.refresh}")
+    private Long refreshTokenExpiration;
+
+    @Value("${auth.jwt.header}")
+    private String header;
+
+    @Value("${auth.jwt.prefix}")
+    private String prefix;
+
+    private final AuthDetailsService authDetailsService;
+
+    public String generateAccessToken(String email) {
+        return Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpiration * 1000))
+                .setSubject(email)
+                .claim("type", "access_token")
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String generateRefreshToken(String email) {
+        return Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpiration * 1000))
+                .setSubject(email)
+                .claim("type", "refresh_token")
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(header);
+        if (bearerToken != null && bearerToken.startsWith(prefix)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey)
+                    .parseClaimsJws(token).getBody().getSubject();
+            return true;
+        } catch (Exception e) {
+            throw new InvalidTokenException();
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        AuthDetails authDetails = authDetailsService.loadUserByUsername(getEmail(token));
+        return new UsernamePasswordAuthenticationToken(authDetails, "", authDetails.getAuthorities());
+    }
+
+    public String getEmail(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secretKey)
+                    .parseClaimsJws(token).getBody().getSubject();
+        } catch (Exception e) {
+            throw new InvalidTokenException();
+        }
+    }
+
+    public boolean isRefreshToken(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secretKey)
+                    .parseClaimsJws(token).getBody().get("type").equals("refresh_token");
+        } catch (Exception e) {
+            throw new InvalidTokenException();
+        }
+    }
+}
