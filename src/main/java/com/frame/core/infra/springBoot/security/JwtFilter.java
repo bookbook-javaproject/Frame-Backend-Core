047c6f34d1a194d60eef6d7e24dc376229e2476e
Maly <migsking@naver.com>
2020-10-31 23:04:14 +0900

    [CREATE] #2 JWT Filter

---
+++ src/main/java/com/frame/core/infra/springBoot/security/JwtFilter.java
@@ -0,0 +1,16 @@
+package com.frame.core.infra.springBoot.security;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class JwtFilter extends GenericFilterBean {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(request, response);
+    }
+}
