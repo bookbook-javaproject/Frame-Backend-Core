c060d56341c1914485e4578d16f07177abc2d1cd
Maly <migsking@naver.com>
2020-11-04 23:25:40 +0900

    [MODIFY] #8 Relationship 추가

--- src/main/java/com/frame/core/domain/user/domain/usecase/CreateUserUseCaseImpl.java
+++ src/main/java/com/frame/core/domain/user/domain/usecase/CreateUserUseCaseImpl.java
@@ -1,16 +1,24 @@
 package com.frame.core.domain.user.domain.usecase;
 
+import com.frame.core.domain.user.domain.entity.Relationship;
 import com.frame.core.domain.user.domain.entity.User;
 import com.frame.core.domain.user.domain.exception.UserAlreadyExistsException;
+import com.frame.core.domain.user.domain.repository.RelationshipRepository;
 import com.frame.core.domain.user.domain.repository.UserRepository;
 import com.frame.core.domain.user.domain.service.PasswordService;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.sun.tools.javac.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
 
 @Component
 @RequiredArgsConstructor
 public class CreateUserUseCaseImpl implements CreateUserUseCase {
     private final UserRepository userRepository;
+    private final RelationshipRepository relationshipRepository;
 
     private final PasswordService passwordService;
 
@@ -28,6 +36,8 @@ public class CreateUserUseCaseImpl implements CreateUserUseCase {
                 .build()
         );
 
-
+        relationshipRepository.save(Relationship.builder().email(email)
+                .follower(Collections.emptyList())
+                .following(Collections.emptyList()).build());
     }
 }
