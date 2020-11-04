52bf192979fdc8b7249f01c2257b13e7c0bbabc7
Maly <migsking@naver.com>
2020-11-04 23:29:38 +0900

    [ADD] #12 권한 설정

--- src/main/java/com/frame/core/infra/springBoot/security/WebSecurityConfig.java
+++ src/main/java/com/frame/core/infra/springBoot/security/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.frame.core.infra.springBoot.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,7 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtProvider jwtProvider;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -17,6 +22,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().disable()
                 .formLogin().disable()
                 .authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers(HttpMethod.POST,"/user").permitAll()
+                .antMatchers(HttpMethod.POST, "/user/auth").permitAll()
+                .antMatchers(HttpMethod.PATCH,"/user/password").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfigure(jwtProvider));
     }
 }
