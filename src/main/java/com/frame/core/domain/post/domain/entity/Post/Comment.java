974a39a392c847c12e211c24e7572235405db661
Maly <migsking@naver.com>
2020-11-06 15:00:34 +0900

    [CREATE] #20 Post Comment Entity

---
+++ src/main/java/com/frame/core/domain/post/domain/entity/Post/Comment.java
@@ -0,0 +1,11 @@
+package com.frame.core.domain.post.domain.entity.Post;
+
+import java.time.LocalDateTime;
+
+public class Comment {
+    private String writer;
+
+    private String comment;
+
+    private LocalDateTime createdAt;
+}
