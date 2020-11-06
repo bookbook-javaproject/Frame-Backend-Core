ebec589837d767716611565b47168459969ad2ec
_uchanlee <uchanlee.dev@gmail.com>
2020-11-06 15:04:00 +0900

    [REFACTOR] #25 Get User UseCase

--- src/main/java/com/frame/core/domain/user/domain/usecase/GetUserUseCase.java
+++ src/main/java/com/frame/core/domain/user/domain/usecase/GetUserUseCase.java
@@ -1,7 +1,6 @@
 package com.frame.core.domain.user.domain.usecase;
 
 import com.frame.core.domain.user.domain.entity.User;
-import com.frame.core.domain.user.dto.GetUserResponse;
 
 public interface GetUserUseCase {
     public User execute(String email);
