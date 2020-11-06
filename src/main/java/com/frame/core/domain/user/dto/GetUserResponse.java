7bfef1b72a71230776928cdb8fa214b274f52154
_uchanlee <uchanlee.dev@gmail.com>
2020-11-06 13:00:45 +0900

    [CREATE] #25 GetUserResponse

---
+++ src/main/java/com/frame/core/domain/user/dto/GetUserResponse.java
@@ -0,0 +1,14 @@
+package com.frame.core.domain.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class GetUserResponse {
+    private String email;
+
+    private String nickname;
+
+    private String imageUri;
+}
