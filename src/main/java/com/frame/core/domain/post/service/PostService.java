76fa42110e09f75e4d256773f1dfdf64df963add
Maly <migsking@naver.com>
2020-11-10 21:24:31 +0900

    [CREATE] #20 Post Service Interface

---
+++ src/main/java/com/frame/core/domain/post/service/PostService.java
@@ -0,0 +1,7 @@
+package com.frame.core.domain.post.service;
+
+import com.frame.core.domain.post.dto.CreatePostRequest;
+
+public interface PostService {
+    public void createPost(CreatePostRequest request);
+}
