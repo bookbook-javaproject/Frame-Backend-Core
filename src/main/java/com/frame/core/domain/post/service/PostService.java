42e4629ee05a303de8c3ecd14f3dc9bb0116a791
Maly <migsking@naver.com>
2020-11-11 11:55:48 +0900

    [ADD] #33 Post Service

--- src/main/java/com/frame/core/domain/post/service/PostService.java
+++ src/main/java/com/frame/core/domain/post/service/PostService.java
@@ -1,7 +1,10 @@
 package com.frame.core.domain.post.service;
 
+import com.frame.core.domain.post.dto.CommentRequest;
 import com.frame.core.domain.post.dto.CreatePostRequest;
 
 public interface PostService {
     public void createPost(CreatePostRequest request);
+
+    public void comment(CommentRequest request);
 }
