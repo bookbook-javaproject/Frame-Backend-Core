86630fd5567ab01c98a9c6c8b95673c127572d20
Maly <migsking@naver.com>
2020-11-11 11:17:27 +0900

    [ADD] #20 Create Post Use Case

--- src/main/java/com/frame/core/domain/post/domain/usecase/CreatePostUseCaseImpl.java
+++ src/main/java/com/frame/core/domain/post/domain/usecase/CreatePostUseCaseImpl.java
@@ -1,29 +1,45 @@
 package com.frame.core.domain.post.domain.usecase;
 
 import com.frame.core.domain.post.domain.entity.Post.Post;
+import com.frame.core.domain.post.domain.entity.Post.PostDetail;
 import com.frame.core.domain.post.domain.entity.Post.enums.AccessType;
 import com.frame.core.domain.post.domain.entity.Post.enums.ContentType;
+import com.frame.core.domain.post.domain.repository.PostDetailRepository;
 import com.frame.core.domain.post.domain.repository.PostRepository;
 import com.frame.core.domain.user.domain.entity.enums.FavoriteType;
 import com.frame.core.global.exceptions.NotFoundException;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 
 
-@RequiredArgsConstructor
+@Component
+@AllArgsConstructor
 public class CreatePostUseCaseImpl implements CreatePostUseCase {
     private final PostRepository postRepository;
 
+    private final PostDetailRepository postDetailRepository;
+
     @Override
     public void execute(String writer, String content, String contentType, String accessType) {
-        postRepository.save(
-                Post.builder()
+        Post post = Post.builder()
                 .content(content)
                 .accessType(generateAccessType(accessType))
                 .contentType(generateContentType(contentType))
                 .createdAt(LocalDateTime.now())
                 .writer(writer)
+                .build();
+
+        postRepository.save(post);
+
+        postDetailRepository.save(
+                PostDetail.builder()
+                .postNumber(post.getPostNumber())
+                .comments(Collections.emptyList())
+                .hearts(Collections.emptyList())
                 .build()
         );
     }
