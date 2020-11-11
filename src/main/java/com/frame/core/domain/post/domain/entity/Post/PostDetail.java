66043b4b978558d22b4b51bf7a540d999b0f446d
Maly <migsking@naver.com>
2020-11-11 11:17:49 +0900

    [MODIFY] #20 Post Detail

--- src/main/java/com/frame/core/domain/post/domain/entity/Post/PostDetail.java
+++ src/main/java/com/frame/core/domain/post/domain/entity/Post/PostDetail.java
@@ -1,14 +1,16 @@
 package com.frame.core.domain.post.domain.entity.Post;
 
+import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@Builder
 @Document(collection = "col_post_detail")
 public class PostDetail {
     @Id
-    private Integer postNumber;
+    private Long postNumber;
 
     private List<String> hearts;
 
