0b51a9e64a6acb220242c3e342c87e6c096f0d87
_uchanlee <uchanlee.dev@gmail.com>
2020-11-12 19:47:15 +0900

    [CREATE] #29 Get Detail Post UseCase Implement

---
+++ src/main/java/com/frame/core/domain/post/domain/usecase/GetDetailPostUseCaseImpl.java
@@ -0,0 +1,31 @@
+package com.frame.core.domain.post.domain.usecase;
+
+import com.frame.core.domain.post.domain.entity.Post.Post;
+import com.frame.core.domain.post.domain.entity.Post.enums.AccessType;
+import com.frame.core.domain.post.domain.repository.PostRepository;
+import com.frame.core.domain.user.domain.exception.UnAuthorizedException;
+import com.frame.core.global.exceptions.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class GetDetailPostUseCaseImpl implements GetDetailPostUseCase {
+    private final PostRepository postRepository;
+
+    @Override
+    public Post execute(String email, Long postNumber) {
+        Optional<Post> optionalPost = postRepository.findById(postNumber);
+
+        optionalPost.ifPresent((
+                post -> {
+                    Boolean isPrivate = post.getAccessType().equals(AccessType.PRIVATE);
+                    if ((isPrivate && !email.equals(post.getWriter()))) {
+                        throw new UnAuthorizedException();
+                    }
+                }));
+        return optionalPost.orElseThrow(NotFoundException::new);
+    }
+}
