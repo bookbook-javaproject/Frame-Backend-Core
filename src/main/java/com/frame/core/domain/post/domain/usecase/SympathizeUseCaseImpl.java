a1d8947d4247e570c4db65e2d26d1cebcfe3c370
Maly <migsking@naver.com>
2020-11-12 11:08:02 +0900

    [CREATE] #35 Sympathize Use Case

---
+++ src/main/java/com/frame/core/domain/post/domain/usecase/SympathizeUseCaseImpl.java
@@ -0,0 +1,25 @@
+package com.frame.core.domain.post.domain.usecase;
+
+
+import com.frame.core.domain.post.domain.repository.PostDetailRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SympathizeUseCaseImpl implements SympathizeUseCase {
+    private final PostDetailRepository postDetailRepository;
+
+    @Override
+    public void execute(String email, Long postId) {
+        postDetailRepository.findById(postId).ifPresent(
+                postDetail -> {
+                    if (postDetail.getHearts().contains(email)) {
+                        postDetail.getHearts().remove(email);
+                    } else {
+                        postDetail.getHearts().add(email);
+                    }
+                }
+        );
+    }
+}
