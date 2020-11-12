def0ad16f05ea1c5938008171a32409e93b5fb40
_uchanlee <uchanlee.dev@gmail.com>
2020-11-12 20:04:14 +0900

    [REFACTOR] #29 Naming

--- src/main/java/com/frame/core/domain/post/domain/usecase/GetPostDetailUseCase.java
+++ src/main/java/com/frame/core/domain/post/domain/usecase/GetPostDetailUseCase.java
@@ -2,6 +2,6 @@ package com.frame.core.domain.post.domain.usecase;
 
 import com.frame.core.domain.post.domain.entity.Post.Post;
 
-public interface GetDetailPostUseCase {
+public interface GetPostDetailUseCase {
     public Post execute(String email, Long postNumber);
 }
