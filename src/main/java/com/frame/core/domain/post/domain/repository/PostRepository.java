7c0982076ba459b30e3ac0b2bf5c3ddcf59e8fae
Maly <migsking@naver.com>
2020-11-12 19:36:40 +0900

    [FIX] #20 Repository bugs

--- src/main/java/com/frame/core/domain/post/domain/repository/PostRepository.java
+++ src/main/java/com/frame/core/domain/post/domain/repository/PostRepository.java
@@ -4,5 +4,5 @@ import com.frame.core.domain.post.domain.entity.Post.Post;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
-    public Iterable<Post> findAllByAccessType_Public();
+    public Iterable<Post> findAllByAccessType(String aPublic);
 }
