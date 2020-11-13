67c5741ac9875b0713e940cd3afd4b5c683224bc
_uchanlee <uchanlee.dev@gmail.com>
2020-11-13 21:22:35 +0900

    [ADD] #20 CreatePostRequest DTO

--- src/main/java/com/frame/core/domain/post/dto/CreatePostRequest.java
+++ src/main/java/com/frame/core/domain/post/dto/CreatePostRequest.java
@@ -2,11 +2,16 @@ package com.frame.core.domain.post.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 public class CreatePostRequest {
+    @NotEmpty
     private String content;
 
+    @NotEmpty
     private String contentType;
 
+    @NotEmpty
     private String accessType;
 }
