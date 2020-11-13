21d86d8f9e3963ee0cebde2f540c657aed6d9ad4
_uchanlee <uchanlee.dev@gmail.com>
2020-11-13 19:57:20 +0900

    [DELETE] #31 Delete Post Detail UseCase

--- src/main/java/com/frame/core/domain/post/domain/usecase/DeletePostDetailUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.frame.core.domain.post.domain.usecase;
-
-import com.frame.core.domain.post.domain.repository.PostRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-public interface DeletePostDetailUseCase {
-    public void execute(String email,Long postId);
-}
