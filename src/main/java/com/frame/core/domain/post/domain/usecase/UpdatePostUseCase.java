587c8266c6a87f8b9d864cdce95a9d38faf4f24a
_uchanlee <uchanlee.dev@gmail.com>
2020-11-15 17:27:02 +0900

    [REFACTOR] #30 Update Post UseCase

--- src/main/java/com/frame/core/domain/post/domain/usecase/UpdatePostUseCase.java
+++ src/main/java/com/frame/core/domain/post/domain/usecase/UpdatePostUseCase.java
@@ -1,7 +1,5 @@
 package com.frame.core.domain.post.domain.usecase;
 
-import com.frame.core.domain.post.dto.UpdatePostRequest;
-
 public interface UpdatePostUseCase {
-    public void execute(String email, UpdatePostRequest request);
+    public void execute(String email, Long postId, String content, String accessType, String contentType);
 }
