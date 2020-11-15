d50a926244038bc8394a40abe753ada6c0771f81
_uchanlee <uchanlee.dev@gmail.com>
2020-11-15 17:28:22 +0900

    [UPDATE] #30 Update Post Request DTO

--- src/main/java/com/frame/core/domain/post/dto/UpdatePostRequest.java
+++ src/main/java/com/frame/core/domain/post/dto/UpdatePostRequest.java
@@ -5,6 +5,15 @@ import lombok.Getter;
 import javax.validation.constraints.NotEmpty;
 
 @Getter
-public class UpdatePostRequest extends CreatePostRequest {
+public class UpdatePostRequest {
     private Long postId;
+
+    @NotEmpty
+    private String content;
+
+    @NotEmpty
+    private String contentType;
+
+    @NotEmpty
+    private String accessType;
 }
