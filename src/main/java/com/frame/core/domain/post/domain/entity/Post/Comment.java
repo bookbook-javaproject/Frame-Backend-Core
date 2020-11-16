57868bf637325883035b9f65527343ef0a7e9e1d
Maly <migsking@naver.com>
2020-11-17 02:49:22 +0900

    [FIX] #56 Comment Getter

--- src/main/java/com/frame/core/domain/post/domain/entity/Post/Comment.java
+++ src/main/java/com/frame/core/domain/post/domain/entity/Post/Comment.java
@@ -1,10 +1,12 @@
 package com.frame.core.domain.post.domain.entity.Post;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Getter
 public class Comment {
     private String writer;
 
