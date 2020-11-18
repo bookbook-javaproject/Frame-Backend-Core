d7d7a27d8fd8d4fa8b45b273b0084e9b04b83a07
_uchanlee <uchanlee.dev@gmail.com>
2020-11-18 10:20:22 +0900

    [MODIFY] #52 PATCH CORS setting

--- src/main/java/com/frame/core/infra/springBoot/security/WebSecurityConfig.java
+++ src/main/java/com/frame/core/infra/springBoot/security/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*");
     }
 }
