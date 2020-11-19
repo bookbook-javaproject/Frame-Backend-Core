89bc0374dfc13a4ec0566b40dd70c916f8e3f652
Maly <migsking@naver.com>
2020-11-20 08:56:03 +0900

    .

---
+++ src/main/java/com/frame/core/domain/notice/service/NoticeService.java
@@ -0,0 +1,7 @@
+package com.frame.core.domain.notice.service;
+
+import com.frame.core.domain.notice.dto.NoticeResponse;
+
+public interface NoticeService {
+    public NoticeResponse getNotices();
+}
