31101a6ac73300c180079e96ad5acc8b8257431e
Maly <migsking@naver.com>
2020-11-20 08:13:24 +0900

    .

---
+++ src/main/java/com/frame/core/domain/submission/domain/repository/AnthologySubmissionRepository.java
@@ -0,0 +1,7 @@
+package com.frame.core.domain.submission.domain.repository;
+
+import com.frame.core.domain.submission.domain.entity.AnthologySubmission;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AnthologySubmissionRepository extends CrudRepository<AnthologySubmission, String> {
+}
