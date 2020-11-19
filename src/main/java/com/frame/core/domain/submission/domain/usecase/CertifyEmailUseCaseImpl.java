2cfb8c46bb959d36569662fe9a1fe98f482d0175
Maly <migsking@naver.com>
2020-11-19 20:14:10 +0900

    급해서 일단 막 올림

---
+++ src/main/java/com/frame/core/domain/submission/domain/usecase/CertifyEmailUseCaseImpl.java
@@ -0,0 +1,20 @@
+package com.frame.core.domain.submission.domain.usecase;
+
+import com.frame.core.domain.submission.domain.entity.AuthorSubmissionAuth;
+import com.frame.core.domain.submission.domain.repository.AuthorSubmissionAuthRepository;
+import com.frame.core.domain.user.domain.entity.EmailAuth;
+import com.frame.core.global.exceptions.NotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CertifyEmailUseCaseImpl implements CertifyEmailUseCase {
+    private final AuthorSubmissionAuthRepository authorSubmissionAuthRepository;
+
+    @Override
+    public void execute(String authCode) {
+        AuthorSubmissionAuth auth = authorSubmissionAuthRepository.findByAuthCode(authCode)
+                .orElseThrow(NotFoundException::new);
+        auth.certify();
+        authorSubmissionAuthRepository.save(auth);
+    }
+}
