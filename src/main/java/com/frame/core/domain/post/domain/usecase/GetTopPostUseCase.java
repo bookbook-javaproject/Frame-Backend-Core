e178de7db460ba736164796ee83694fe94f6996a
Maly <migsking@naver.com>
2020-11-20 22:05:40 +0900

    [CREATE] GetTopPostUseCase

--- src/main/java/com/frame/core/domain/post/domain/usecase/GetTopPostUseCase.java
+++ src/main/java/com/frame/core/domain/post/domain/usecase/GetTopPostUseCase.java
@@ -2,6 +2,8 @@ package com.frame.core.domain.post.domain.usecase;
 
 import com.frame.core.domain.post.domain.entity.Post.Post;
 
+import java.util.List;
+
 public interface GetTopPostUseCase {
-    public Post execute();
+    public List<Long> execute();
 }
--- src/main/java/com/frame/core/domain/post/domain/usecase/GetTopPostUseCaseImpl.java
+++ src/main/java/com/frame/core/domain/post/domain/usecase/GetTopPostUseCaseImpl.java
@@ -1,11 +1,15 @@
 package com.frame.core.domain.post.domain.usecase;
 
 import com.frame.core.domain.post.domain.entity.Post.Post;
+import com.frame.core.domain.post.domain.entity.Post.PostDetail;
 import com.frame.core.domain.post.domain.repository.PostDetailRepository;
 import com.frame.core.domain.post.domain.repository.PostRepository;
 import com.frame.core.global.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Component
@@ -15,9 +19,13 @@ public class GetTopPostUseCaseImpl implements GetTopPostUseCase {
     private final PostDetailRepository postDetailRepository;
 
     @Override
-    public Post execute() {
-        return postRepository.findById(
-                postDetailRepository.findTop10ByOrderByHearts().getPostNumber()
-        ).orElseThrow(NotFoundException::new);
+    public List<Long> execute() {
+        List<Long> postIds = new ArrayList<Long>();
+
+        for (PostDetail postDetail: postDetailRepository.findTop10ByOrderByHearts()) {
+            postIds.add(postDetail.getPostNumber());
+        }
+
+        return postIds;
     }
 }
