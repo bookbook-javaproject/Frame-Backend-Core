6598d9c0538a2f1411b8fc523f7bacd4e8137575
_uchanlee <uchanlee.dev@gmail.com>
2020-12-01 11:30:10 +0900

    Update GetUserPostsResponse.java

--- src/main/java/com/frame/core/domain/post/dto/GetUserPostsResponse.java
+++ src/main/java/com/frame/core/domain/post/dto/GetUserPostsResponse.java
@@ -5,11 +5,9 @@ import lombok.Getter;
 
 import java.util.List;
 
-import com.frame.core.domain.user.domain.entity.User;
 
 @Builder
 @Getter
 public class GetUserPostsResponse {
     private List<UserPostPreview> userPostPreviews;
-    private User writer;
 }
