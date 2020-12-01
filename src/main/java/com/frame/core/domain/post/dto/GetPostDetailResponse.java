072d2651549c546fbab8f556e7bfcdc6e9a4a471
_uchanlee <uchanlee.dev@gmail.com>
2020-12-01 21:24:57 +0900

    Update GetPostDetailResponse.java

--- src/main/java/com/frame/core/domain/post/dto/GetPostDetailResponse.java
+++ src/main/java/com/frame/core/domain/post/dto/GetPostDetailResponse.java
@@ -22,4 +22,8 @@ public class GetPostDetailResponse {
     private List<String> hearts;
 
     private List<CommentPreview> comments;
+    
+    private String accessType;
+    
+    private String contentType;
 }
