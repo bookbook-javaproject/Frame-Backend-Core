217c559e94cf005f2add56e5489b7dc89f7b5211
_uchanlee <uchanlee.dev@gmail.com>
2020-12-02 16:53:58 +0900

    Update UserServiceImpl.java

--- src/main/java/com/frame/core/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/frame/core/domain/user/service/UserServiceImpl.java
@@ -52,6 +52,7 @@ public class UserServiceImpl implements UserService {
                 .nickname(user.getNickname())
                 .imageUri(user.getImageUri())
                 .description(user.getDescription())
+                .userType(user.getUserType() != null ? String.valueOf(user.getUserType().name()) : null)
                 .build();
     }
 
